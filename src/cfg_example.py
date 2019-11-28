9211ee75d99d8802f05219b3ceda90723e383a2a
gloryvictory <zamaraev@gmail.com>
2019-11-28 17:38:16 +0500

    init

---
+++ src/cfg_example.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+#   Author          : Viacheslav Zamaraev
+#   email           : zamaraev@gmail.com
+#   Script Name     : cfg.py
+#   Created         : 25th November 2019
+#   Last Modified	: 25th November 2019
+#   Version		    : 1.0
+#   PIP             :
+#   RESULT          :
+# Modifications	: 1.1 -
+#               : 1.2 -
+#
+# Description   : just rename cfg_example to cfg.py
+
+DADATA_KEY = ""
+DADATA_SECRET = ""
---
+++ src/test_dadata.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+#   Author          : Viacheslav Zamaraev
+#   email           : zamaraev@gmail.com
+#   Script Name     : test_dadata.py
+#   Created         : 25th November 2019
+#   Last Modified	: 25th November 2019
+#   Version		    : 1.0
+#
+# Modifications	: 1.1 -
+#               : 1.2 -
+#
+# Description   : This script will search any *.shp files in the given directory and convert to KML files
